dc87ce2bc734f752ff05e728fc8e31a81113401e
whistle <435424527@qq.com>
2023-10-12 17:27:14 +0800

    202310121727

--- 09 Function/_2_closures/src/test4.rs
+++ 09 Function/_2_closures/src/test4.rs
@@ -1,6 +1,18 @@
 pub fn test4 () {
     let a = "".to_string();
-    let f1 = || a ;
-    ;
+    let f1 = move || {println!("{}", a); std::mem::drop(a)};
+    // println!("{}",a);
 
+    // let a = 0;
+    // let f1 = move || println!("{}", a);
+
+    let mut b = "".to_string();
+    let mut f2 = move || {b+="x"; println!("{}", b);};
+    f2();
+    f2();
+    // println!("{}", b);
+
+    let c = "".to_string();
+    let f3 = || c;
+    f3();
 }
